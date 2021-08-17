25139f580a456692c8879d1e80d66756698a3a62
g-prog <alaobukky2@gmail.com>
2021-08-17 05:42:22 -0700

    working

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,26 +1,32 @@
 import "./intro.scss"
-import { ExpandMore } from "@material-ui/icons"
+
+
 
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-                <div className=" md:flex flex-row justify-around">
+                <div className=" md:flex flex-row item-center justify-around">
                     <div className="pt-12 md:pt-0 mt-0 lg:pt-20  ">
-                        <div className="bg-red-600 bg-opacity-70 lg:w-max  rounded-full">
+                        <div className="bg-red-700  md:mt-7  bg-opacity-70 lg:w-max  rounded-full">
                             <div className="picture">
                                 <img src="assets/buky.png" className="pic" alt="Buky"/>
                             </div>
                         </div>
                     </div>
-                    <div className="mt-10 md:mt-20 lg:mt-40">
+                    <div className="mt-7 md:mt-8 lg:mt-40 pl-0 md:pl-14 lg:pl-0 relative overflow-y-hidden">
                         <div className="wrapper">
-                            <h2 className="font-bold text-3xl">Hi There,I'm<br></br>
-                            <span className="font-bold text-5xl" >Alao Adebukola</span><br></br>Experienced<span></span></h2>
+                            <h2 className="font-bold text-3xl">Hi There, I'm<br></br>
+                            <span className="font-bold text-5xl" >Alao Adebukola</span><br></br>Experienced<span className="text-red-700">  Web Developer</span></h2>
+
 
                         </div>
-                        <a href="#portfolio">
-                            <ExpandMore/>
-                        </a>
+                        <div className="arrow"> 
+                            <a href="#portfolio" className="bottom-0 absolute lg:bottom-0.5 ml-28 lg:ml-24">
+                                
+                                <img src="assets/down.jpg" alt="arrow"/>
+                            </a>
+                        </div>
+                        
                     </div>
                    
                 </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,78 @@
-import "./portfolio.scss"
+import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useState } from "react";
 
 export default function Portfolio() {
+    const  [selected, setSelected] = useState("featured");
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+            
+        },
+
+
+        {
+            id: "django",
+            title: "Django App",
+            
+        },
+
+        {
+            id: "react",
+            title: "React App",
+            
+        },
+
+        {
+            id: "next",
+            title: "Next App",
+            
+        },
+
+        {
+            id: "other",
+            title: "Other Works",
+            
+        },
+    ]
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map(item=>(
+                    <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected}
+                    id={item.id }/>
+                ))}
+
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/caj.jpg" alt="caj" />
+                    <h3>Blog App</h3>   
+                </div>
+                <div className="item">
+                    <img src="assets/caj.jpg" alt="caj" />
+                    <h3>Blog App</h3>  
+                </div>
+                <div className="item">
+                    <img src="assets/caj.jpg" alt="caj" />
+                    <h3>Blog App</h3> 
+                </div>
+
+                <div className="item">
+                    <img src="assets/caj.jpg" alt="caj" />
+                    <h3>Blog App</h3> 
+                </div>
+                <div className="item">
+                    <img src="assets/caj.jpg" alt="caj" />
+                    <h3>Blog App</h3> 
+                </div>
+                <div className="item">
+                    <img src="assets/caj.jpg" alt="caj" />
+                    <h3>Blog App</h3> 
+                </div>
+            </div>
             
             
             
