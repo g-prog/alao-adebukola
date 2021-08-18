183c2a2b45bbe4fa4aae370a44afd2f62a7140a4
g-prog <alaobukky2@gmail.com>
2021-08-18 12:05:31 -0700

    works

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -6,8 +6,24 @@ export default function Works() {
             <div className="slider">
                 <div className="container">
                     <div className="item">
-                        <div className="left"></div>
-                        <div className="right"></div>
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/react.png" alt="react logo" />
+                                </div>
+                                <h2>Meck Health</h2>
+                                <p>I designed this project with the help of React.js, Tailwind CSS,SASS, 
+                                    HTML and CSS.
+                                    I implemented toast notifications with the help of React toastify and also 
+                                    utilized the JavaScript Geolocation API in order to access the user location.
+                                    I also used the useState Hook and useEffect Hook to make the contents dynamic.
+                                </p>
+                                <span><a href="https://health-nine.vercel.app/">Link to project</a></span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <img src="assets/meck.png" alt="meck" />
+                        </div>
                     </div>
                 </div>
             </div>
