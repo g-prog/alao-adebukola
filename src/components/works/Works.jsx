775f07af5ebddaf29531ebdd9c9899d5d653d86d
g-prog <alaobukky2@gmail.com>
2021-08-19 08:00:09 -0700

    slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,36 +1,91 @@
-import "./works.scss"
+
+import { useState } from "react";
+import "./works.scss";
 
 export default function Works() {
-    return (
-        <div className="works" id="works">
-            <div className="slider">
-                <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imgContainer">
-                                    <img src="assets/react.png" alt="react logo" />
-                                </div>
-                                <h2>Meck Health</h2>
-                                <p>I designed this project with the help of React.js, Tailwind CSS, Material UI, SASS, 
-                                    HTML and CSS.
-                                    I implemented toast notifications with the help of React toastify and also 
-                                    utilized the JavaScript's Geolocation API in order to access the user's location.
-                                    I also used the useState Hook and useEffect Hook to make the contents dynamic.
-                                </p>
-                                <span><a href="https://health-nine.vercel.app/">Link to project</a></span>
-                            </div>
-                        </div>
-                        <div className="right">
-                            <img src="assets/meck.png" alt="meck" />
-                        </div>
-                    </div>
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+        id: 1,
+        icon: "../assets/react.png",
+        title:"Meck App", 
+        img: "../assets/meck.png",
+        desc: "Built with the help of React.js, Tailwind CSS, Material UI, SASS, HTML and CSS. I utilized and implemented the Geolocation API, useState and useEffect hooks to access the user's location and make the contents dynamic. I also used react toastify to create toast notifications.",
+        link: "https://health-nine.vercel.app/"
+
+    },
+
+    {
+        id: 2,
+        icon: "../assets/react.png",
+        title:"ABCD Website", 
+        img: "../assets/abcd.png",
+        desc: "Built the About page section of the ABCD website with the help of React.js, Tailwind CSS, Material UI,  HTML and CSS and Next.js.",
+        link: "https://abcd-website.vercel.app/about"
+
+
+
+    },
+
+    {
+        id: 3,
+        icon: "../assets/react.png",
+        title:"Vasiti Website", 
+        img: "../assets/vasiti.png",
+        desc: "Built with the help of React.js, Tailwind CSS, Material UI, SASS, HTML, CSS.",
+        link: "https://template-nine-xi.vercel.app/"
+
+    },
+
+  ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+  
+  return (
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>{d.link}</span>
                 </div>
-                <img src="assets/left.png" alt="arrow" className="arrow left"/>
-                <img src="assets/left.png" alt="arrow" className="arrow right "/>
+              </div>
+              <div className="right">
+                <img
+                  src={d.img}
+                  alt=""
+                />
+              </div>
             </div>
-           
-            
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/left.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/left.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
