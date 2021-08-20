b46f60336b23b52ef7fad8aabad99578a28a677c
g-prog <alaobukky2@gmail.com>
2021-08-20 07:11:13 -0700

    created dynamic text using ityped

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,20 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 
 
 export default function Intro() {
+    const textRef = useRef();
+
+   useEffect(()=>{
+       init(textRef.current,{
+           showCursor: 'false',
+           strings: ["Front-end Developer", " in React.js,  Next.js", "Tailwind CSS, Bootstrap" ,"HTML,CSS,SASS", "rendering API's", "Material UI, Fontawesome"]
+       } )
+
+
+   }, [])
     return (
         <div className="intro" id="intro">
                 <div className="left">
@@ -14,7 +26,7 @@ export default function Intro() {
                     <div className="wrapper">
                         <h2 className="font-bold text-2xl">Hi There,I am</h2>
                         <h1 >Alao Adebukola</h1>
-                        <h3 className="font-bold text-2xl">Experienced<span></span></h3>
+                        <h3 className="font-bold text-2xl">Experienced  <span ref={textRef}></span></h3>
                     </div>
 
                     <a href="#portfolio">
