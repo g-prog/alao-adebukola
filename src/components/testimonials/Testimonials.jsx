485555e2d28b8aebd3830306eb61cfc1111b6a66
g-prog <alaobukky2@gmail.com>
2021-08-27 15:36:35 -0700

    working on work experience section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,9 +3,19 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
+           
             <div className="card">
                 <div className="circle"></div>
-                <div className="content"></div>
+                <div className="content">
+                    <h6>Africa's Blockchain Center for Developers</h6>
+                    <p>Web Developer</p>
+                    <p>Assisted in the development of front-end web applications using React and Next.js,
+                        HTML, CSS, Bootstrap and Tailwind CSS
+                    </p>
+                    <p>Ran transactions in the Cardano Testnet with the use of Dandelion APIs</p>
+                    <p>Performed blockchain queries using Axios, GraphQL and Dandelion APIs</p>
+                    <p>Created terminal based applications with Haskell in the backend</p>
+                </div>
                 <img src="assets/html.jpg" alt="caj" />
             </div>
            
