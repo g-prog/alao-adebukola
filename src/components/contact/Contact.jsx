36f1a54d1a5d1d7345ce2858eaa5f56e51dddd87
g-prog <alaobukky2@gmail.com>
2021-08-27 13:47:06 -0700

    adding a work experience layer and finished the contact layer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,26 @@
 import "./contact.scss"
+import { useState } from "react";
 
 export default function Contact() {
+    const [message, setMessage] = useState(false)
+    const handlesubmit = (e)=> {
+        e.preventDefault();
+        setMessage(true)
+    }
     return (
         <div className="contact" id="contact">
+            <div className="left">
+                <img src="assets/shake.png" alt="contact" />
+            </div>
+            <div className="right">
+                <h2>Contact Me.</h2>
+                <form onSubmit={handlesubmit}>
+                    <input type="text"  placeholder="Email"/>
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks for reaching out, I'll reply shortly</span>}
+                </form>
+            </div>
             
             
         </div>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,6 +3,11 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
+            <div className="card">
+                <div className="circle"></div>
+                <div className="content"></div>
+                <img src="assets/html.jpg" alt="caj" />
+            </div>
            
             
         </div>
