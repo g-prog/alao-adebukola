2f125685ff5156d41cac5b4efe9659ac7aab48fa
g-prog <alaobukky2@gmail.com>
2021-08-28 06:14:23 -0700

    finished with work experience section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,6 +3,10 @@ import "./testimonials.scss"
 export default function Testimonials() {
     return (
         <div className="testimonials" id="testimonials">
+            <div className="cta">
+                <p>hover the images</p>
+            </div>
+           
            <div className="heading">
                 <h1>Work Experience</h1>
            </div>
@@ -11,17 +15,51 @@ export default function Testimonials() {
                     <div className="circle"></div>
                     <div className="content">
                         <h6>Africa's Blockchain Center for Developers</h6>
-                        <p>Web Developer</p>
-                        <p>Assisted in the development of front-end web applications using React and Next.js,
-                            HTML, CSS, Bootstrap and Tailwind CSS
-                        </p>
-                        <p>Ran transactions in the Cardano Testnet with the use of Dandelion APIs</p>
-                        <p>Performed blockchain queries using Axios, GraphQL and Dandelion APIs</p>
-                        <p>Created terminal based applications with Haskell in the backend</p>
+                        <h6>October 2020-May 2021</h6>
+                        <h6>Web Developer</h6>
+                        <div className="item">
+                            <div className="item1">
+                            <p>Assisted in the development of front-end web applications using React and Next.js,
+                            HTML, CSS, Bootstrap and Tailwind CSS.
+                            </p>
+                            </div>
+                            <div className="item2">
+                                <p>Ran transactions in the Cardano Testnet with the use of Dandelion APIs.</p>
+                            </div>
+                            <div className="item3">
+                                <p>Created terminal based applications with Haskell in the backend.</p>
+                            </div>
+                        </div>
+                        
                     </div>
                     <img src="assets/html.jpg" alt="caj" />
                 </div>
-            
+
+
+                <div className="card2">
+                    <div className="circle"></div>
+                    <div className="content">
+                        <h6>Build for SDG's powered by Andela</h6>
+                        <h6>April 2020 - October 2020</h6>
+                        <h6>Intern</h6>
+                        <div className="item">
+                            <div className="item1">
+                            <p>Assisted in the development of a Web app that centered on Agriculture.
+                            </p>
+                            </div>
+                            <div className="item2">
+                            <h6>Google Africa Developer Scholarship</h6>
+                             <h6>Mentor</h6>
+                             <p>Provided Guidance to team members in the development of web based applications.</p>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    <img src="assets/html.jpg" alt="caj" />
+                </div>
+
+                
             </div>
             
         </div>
