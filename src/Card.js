4431c55f6efe6570e6ad5bba42ac9779d8e87798
g-prog <alaobukky2@gmail.com>
2022-01-13 04:36:04 -0800

    still trying to make progress with the anchor tags

--- src/Card.js
+++ src/Card.js
@@ -8,8 +8,8 @@ function Card({ src, title, description, link, github }) {
             <div className="card__info">
                 <h2>{title}</h2>
                 <h4>{description}</h4>
-                 <a href={link}>Project</a>
-                 <a href={github}>Github</a>
+                <a href={link}> Hosted Project</a>
+                <a href={github}>Github</a>
                 
             </div>
         </div>
