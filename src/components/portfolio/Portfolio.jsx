5b87d0bb2fe274b8b822dd06e5c723c2763dcd5f
g-prog <alaobukky2@gmail.com>
2022-04-19 09:45:51 -0700

    worked on some issues

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,107 +1,97 @@
 import "./portfolio.scss";
 import PortfolioList from "../portfolioList/PortfolioList";
 import { useState, useEffect } from "react";
-import { featuredportFolio, djangoportFolio, reactportFolio, nextportFolio, otherportFolio } from "../../data";
+import {
+  featuredportFolio,
+  djangoportFolio,
+  reactportFolio,
+  nextportFolio,
+  otherportFolio,
+} from "../../data";
 
 export default function Portfolio() {
-    const  [selected, setSelected] = useState("featured");
-    const  [data, setData] = useState([]);
-    const list = [
-        {
-            id: "featured",
-            title: "Featured",
-            
-        },
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
 
+    {
+      id: "django",
+      title: "Django App",
+    },
 
-        {
-            id: "django",
-            title: "Django App",
-            
-        },
+    {
+      id: "react",
+      title: "React App",
+    },
 
-        {
-            id: "react",
-            title: "React App",
-            
-        },
+    {
+      id: "next",
+      title: "Next App",
+    },
 
-        {
-            id: "next",
-            title: "Next App",
-            
-        },
+    {
+      id: "other",
+      title: "Other Works",
+    },
+  ];
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredportFolio);
+        break;
 
-        {
-            id: "other",
-            title: "Other Works",
-            
-        },
-    ]
-    useEffect (()=>{
-        switch(selected){
-                case "featured":
-                    setData(featuredportFolio);
-                    break;
+      case "django":
+        setData(djangoportFolio);
+        break;
 
-                case "django":
-                    setData(djangoportFolio);
-                    break;
+      case "react":
+        setData(reactportFolio);
+        break;
 
-                case "react":
-                    setData(reactportFolio);
-                    break;
+      case "next":
+        setData(nextportFolio);
+        break;
 
-                case "next":
-                    setData(nextportFolio);
-                    break;
+      case "other":
+        setData(otherportFolio);
+        break;
 
-                case "other":
-                    setData(otherportFolio);
-                    break;
+      default:
+        setData(featuredportFolio);
+    }
+  }, [selected]);
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="caj" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
+      </div>
 
-                default:
-                    setData(featuredportFolio);
-
-
-
-        }
-    }, [selected])
-    return (
-        <div className="portfolio" id="portfolio">
-            <h1>Portfolio</h1>
-            <ul>
-                {list.map(item=>(
-                    <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected}
-                    id={item.id }/>
-                ))}
-
-            </ul>
-            <div className="container">
-                {data.map(d=>(
-
-                    <div className="item">
-                    <img src={d.img} alt="caj" />
-                    <h3>{d.title}</h3>   
-                    </div>
-
-                ))}
-                
-                
-            </div>
-
-           <div className="cta">
-                <p>slide to see full details  below</p>
-                <a href="#works">
-                    <img src="assets/down.jpg" alt="down arrow" />
-                </a>
-           </div>
-
-           
-           
-            
-            
-            
-        </div>
-    )
+      <div className="cta">
+        <p>slide to see full details below</p>
+        <a href="#works">
+          <img src="assets/down.jpg" alt="down arrow" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,32 +1,32 @@
-import "./topbar.scss"
-import {Person, Mail} from "@material-ui/icons"
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar({menuOpen, setMenuOpen}) {
-    return (
-        <div className={"topbar " + (menuOpen && "active")} id="topbar">
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro" className="logo">Adebukola.</a>
-                    <div className="itemContainer1">
-                        <Person className="icon"/>
-                        <span>+2348122213918</span>
-                    </div>
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")} id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Adebukola.
+          </a>
+          <div className="itemContainer1">
+            <Person className="icon" />
+            <span>+2348122213918</span>
+          </div>
 
-                    <div className="itemContainer2">
-                        <Mail className="icon"/>
-                        <span>alaoadebukola6@gmail.com</span>
-                    </div>
-                </div>
-                <div className="right">
-                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                    
-                </div>
-            </div>
-            
+          <div className="itemContainer2">
+            <Mail className="icon" />
+            <span>alaoadebukola6@gmail.com</span>
+          </div>
         </div>
-    )
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
